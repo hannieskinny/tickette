f75cf02c09b04fce1adbd99c7537dd1209ae1f2d
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-05 17:54:12 -0500

    Create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickette::Application.config.secret_key_base = 'f0a56351dc8c81070b6ea4a544dc03f72a71b5142d48c01884562adfb172e6abdaa2d47c255455a8da21d42157abbe46635821f9ec21bef88872b923b65e1133'
